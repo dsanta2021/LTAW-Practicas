f127a1c3fad75106fcb10fdf75a994691d8a3e0f
dsanta2021 <d.santa.2021@alumos.urjc.es>
2025-02-17 13:41:48 +0100

    server http

--- P1/tienda.js
+++ P1/tienda.js
@@ -1,3 +1,26 @@
-document.addEventListener("DOMContentLoaded", () => {
-    console.log("Tienda Friki cargada");
+const http = require('http');
+
+//-- Especificaciones
+const PUERTO = 8001;
+
+//-- Crear el servidor
+const server = http.createServer((req, res) => {
+    
+  //-- Indicamos que se ha recibido una petición
+  console.log("Petición recibida!");
+
+  //-- Cabecera que indica el tipo de datos del
+  //-- cuerpo de la respuesta: Texto plano
+  res.setHeader('Content-Type', 'text/plain');
+
+  //-- Mensaje del cuerpo
+  res.write("Soy el Happy server!!\n");
+
+  //-- Terminar la respuesta y enviarla
+  res.end();
 });
+
+//-- Activar el servidor: ¡Que empiece la fiesta!
+server.listen(PUERTO);
+
+console.log("Happy server activado!. Escuchando en puerto: " + PUERTO);
